80411b5c57d890c6405f9dd7bfc9f097f903d365
AlejandraBV <a.bravo@uniandes.edu.co>
2021-12-03 13:47:43 -0500

    Inicio

---
+++ smarthomeapp/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Space from './components/space';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My spaces</h1>
+      <Space/>
+    </div>
+  );
+}
+
+export default App;
