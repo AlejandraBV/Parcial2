e9f2743aba670cf18c33904686cf472a4389b4ca
AlejandraBV <a.bravo@uniandes.edu.co>
2021-12-06 21:45:06 -0500

    Finalizado 100%realnofake

--- smarthomeapp/src/components/room/room.js
+++ smarthomeapp/src/components/room/room.js
@@ -11,14 +11,19 @@ class Room extends React.Component {
     rooms: [],
     rClicked: false,
     cRoom: [],
+    online: true,
   };
 
   async componentDidMount() {
+    if (!window.navigator.onLine) {
+      this.setState({ online: false });
+    } else {
       const url =
         "https://gist.githubusercontent.com/josejbocanegra/92c90d5f2171739bd4a76d639f1271ea/raw/9effd124c825f7c2a7087d4a50fa4a91c5d34558/rooms.json";
       const res = await fetch(url);
       const data = await res.json();
       this.setState({ rooms: data });
+    }
   }
 
   handleClick(sRooms, roomID) {
@@ -33,54 +38,68 @@ class Room extends React.Component {
 
     return (
       <div>
-        <h1 className="left"><FormattedMessage id="MyRooms"/></h1>
-        <div className="container">
-          <div className="row">
-            <div className="col-6">
-              <div className="grid-container">
-                {sRoom.map((item, i) => (
-                  <div
-                    className="card-room clicked"
-                    onClick={() => {
-                      this.handleClick(sRoom, i);
-                    }}
-                    key={i}
-                  >
-                    <div className="normal-align">
-                      <p className="title">{item.name}</p>
-                    </div>
-                    <img src={imgs[i]} alt="" width="120" />
+        <h1 className="left">
+          <FormattedMessage id="MyRooms" />
+        </h1>
+        {window.navigator.onLine === true ? (
+          <div>
+            <div className="container">
+              <div className="row">
+                <div className="col-6">
+                  <div className="grid-container">
+                    {sRoom.map((item, i) => (
+                      <div
+                        className="card-room clicked"
+                        onClick={() => {
+                          this.handleClick(sRoom, i);
+                        }}
+                        key={i}
+                      >
+                        <div className="normal-align">
+                          <p className="title">{item.name}</p>
+                        </div>
+                        <img src={imgs[i]} alt="" width="120" />
+                      </div>
+                    ))}
                   </div>
-                ))}
+                </div>
+                <div className="col-6">
+                  {this.state.rClicked === true && (
+                    <table id="customers">
+                      <thead>
+                        <tr>
+                          <th>#</th>
+                          <th>ID</th>
+                          <th>
+                            <FormattedMessage id="Device" />
+                          </th>
+                          <th>
+                            <FormattedMessage id="Value" />
+                          </th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {this.state.cRoom.devices.map((item, i) => (
+                          <tr key={item.id}>
+                            <td>{i + 1}</td>
+                            <td>{item.id}</td>
+                            <td>{item.name}</td>
+                            <td>{item.desired.value}</td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+                  )}
+                </div>
               </div>
             </div>
-            <div className="col-6">
-              {this.state.rClicked === true && (
-                <table id="customers">
-                  <thead>
-                    <tr>
-                      <th>#</th>
-                      <th>ID</th>
-                      <th><FormattedMessage id="Device"/></th>
-                      <th><FormattedMessage id="Value"/></th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {this.state.cRoom.devices.map((item, i) => (
-                      <tr key={item.id}>
-                        <td>{i + 1}</td>
-                        <td>{item.id}</td>
-                        <td>{item.name}</td>
-                        <td>{item.desired.value}</td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              )}
-            </div>
+            <Pie pData={sRoom} />
           </div>
-        </div>
-        <Pie pData={sRoom} />
+        ) : (
+          <h1>
+            <FormattedMessage id="Loading" />
+          </h1>
+        )}
       </div>
     );
   }
--- smarthomeapp/src/components/space/space.js
+++ smarthomeapp/src/components/space/space.js
@@ -9,37 +9,52 @@ import { FormattedMessage } from "react-intl";
 class Space extends React.Component {
   state = {
     states: [],
+    online: true,
   };
 
   async componentDidMount() {
+    console.log(window.navigator.onLine);
+    if (!navigator.onLine) {
+      this.setState({ online: false });
+      console.log("entré, stonks");
+    } else {
       const url =
         "https://gist.githubusercontent.com/josejbocanegra/0067d2b28b009140fee423cfc84e40e6/raw/6e6b11160fbcacb56621b6422684d615dc3a0d33/spaces.json";
       const res = await fetch(url);
       const data = await res.json();
       this.setState({ states: data });
+    }
   }
 
   render() {
     const imgs = [img0, img1, img2, img3];
     return (
       <div>
-        <h1><FormattedMessage id="MySpaces"/></h1>
+        <h1>
+          <FormattedMessage id="MySpaces" />
+        </h1>
         <div className="container">
-          {this.state.states.map((item, i) => (
-            <div
-              className="card clicked"
-              onClick={() => {
-                this.props.handleClick(item.id);
-              }}
-              key={item.id}
-            >
-              <img src={imgs[i]} alt="" width="120" />
-              <div className="normal-align">
-                <p className="title">{item.name}</p>
-                <p>{item.address}</p>
+          {this.state.online === true ? (
+            this.state.states.map((item, i) => (
+              <div
+                className="card clicked"
+                onClick={() => {
+                  this.props.handleClick(item.id);
+                }}
+                key={item.id}
+              >
+                <img src={imgs[i]} alt="" width="120" />
+                <div className="normal-align">
+                  <p className="title">{item.name}</p>
+                  <p>{item.address}</p>
+                </div>
               </div>
-            </div>
-          ))}
+            ))
+          ) : (
+            <h1>
+              <FormattedMessage id="Loading" />
+            </h1>
+          )}
         </div>
       </div>
     );
